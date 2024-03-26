3f1b509f6ad2683c58cb47f8c42ebb037fd90d7c
ilya-nuhi <92297205+ilya-nuhi@users.noreply.github.com>
2024-03-26 23:59:03 +0300

    levelmanager fixed

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
@@ -43,4 +44,9 @@ public class LevelManager : MonoBehaviour
             yield return new WaitForSecondsRealtime(creationDelay);
         }
     }
+
+    public void StartLevel(){
+        currentLevel = PlayerPrefs.GetInt(CurrentLevel,0);
+        SceneManager.LoadScene("Game");
+    } 
 }
